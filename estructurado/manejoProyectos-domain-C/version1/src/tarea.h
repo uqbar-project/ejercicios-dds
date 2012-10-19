145f5f8b193dd6dde1e358258455336200031cc6
fdodino <fdodino>
2012-10-19 23:01:50 +0000

    Version 1: El test conoce la interfaz de impuesto y complejidad, aunque sólo para generar el juego de datos
    git-svn-id: https://xp-dev.com/svn/uqbar/examples/dds@611 e1a88654-4610-4beb-b2e6-8ed2f74c6698

---
+++ estructurado/manejoProyectos-domain-C/version1/src/tarea.h
@@ -0,0 +1,34 @@
+/*
+ * tarea.h
+ *
+ *  Created on: 26/05/2012
+ *      Author: dodain
+ */
+
+#ifndef TAREA_H_
+#define TAREA_H_
+
+#include "complejidad.h"
+#include "impuesto.h"
+
+// definición de estructuras
+typedef struct Tarea t_tarea;
+typedef struct t_tarea tad_tarea;
+typedef t_tarea* tarea;
+
+/************************************************************
+* Operaciones primitivas sobre el TAD Tarea
+*************************************************************/
+tarea Tarea_crear(char*, int, t_complejidad);
+// TODO: Definir la asignación de una complejidad minima, media y maxima
+float Tarea_costo(tarea);
+float Tarea_tiempoTotal(tarea);
+char* Tarea_asString(tarea);
+void Tarea_agregarSubtarea(tarea, tarea);
+void Tarea_agregarImpuesto(tarea, impuesto);
+void Tarea_destroy(tarea);
+
+// temp
+void Tarea_verSubtareas(tarea);
+
+#endif /* TAREA_H_ */
---
+++ estructurado/manejoProyectos-domain-C/version1/tst/tareaTest.c
@@ -0,0 +1,107 @@
+/*
+ * testTarea.c
+ *
+ *  Created on: 26/05/2012
+ *      Author: dodain
+ */
+#include <assert.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../src/complejidad.h"
+#include "../src/tarea.h"
+#include "../src/impuesto.h"
+#include "../src/proyecto.h"
+//#include <cutest-1.5/CuTest.h>
+#include "CuTest.h"
+#include "tareaTest.h"
+
+/********************************************************************
+ *  FUNCIONES INTERNAS
+ *********************************************************************/
+static tarea prepareDarClase(void) {
+	tarea darClase;
+	tarea prepararClase;
+	tarea medirTiempoClase;
+	darClase = Tarea_crear("Dar clase de Diseño Estructurado", 8, COMPLEJIDAD_MINIMA);
+	prepararClase = Tarea_crear("Preparar clase", 5, COMPLEJIDAD_MINIMA);
+	medirTiempoClase = Tarea_crear("Medir tiempo clase", 3, COMPLEJIDAD_MEDIA);
+	Tarea_agregarSubtarea(darClase, prepararClase);
+	Tarea_agregarSubtarea(darClase, medirTiempoClase);
+	return darClase;
+}
+
+static float costoTarea(char *descripcion, int tiempo, t_complejidad complejidad) {
+	tarea tarea = Tarea_crear(descripcion, tiempo, complejidad);
+	float costo = Tarea_costo(tarea);
+	Tarea_destroy(tarea);
+	return costo;
+}
+
+/********************************************************************
+ *  TESTS
+ *********************************************************************/
+void TestCostoTareaSimpleComplejidadMinima(CuTest* tc) {
+	tarea prepararClase;
+	prepararClase = Tarea_crear("Preparar clase", 5, COMPLEJIDAD_MINIMA);
+	float costoDarClase = Tarea_costo(prepararClase);
+
+	CuAssert(tc, "costo de preparar clase", costoDarClase == 125);
+}
+
+void TestCostoTareaSimpleComplejidadMinimaConImpuestos(CuTest* tc) {
+	tarea prepararClase;
+	prepararClase = Tarea_crear("Preparar clase", 5, COMPLEJIDAD_MINIMA);
+	impuesto impuestoA;
+	impuesto impuestoB;
+	impuestoA = Impuesto_crear("Impuesto A", 3.0f);
+	impuestoB = Impuesto_crear("Impuesto B", 5.0f);
+	Tarea_agregarImpuesto(prepararClase, impuestoA);
+	Tarea_agregarImpuesto(prepararClase, impuestoB);
+	float costoPrepararClase = Tarea_costo(prepararClase);
+	Impuesto_destroy(impuestoA);
+	Impuesto_destroy(impuestoB);
+	Tarea_destroy(prepararClase);
+
+	CuAssert(tc, "costo de preparar clase", costoPrepararClase == 135);
+}
+
+void TestCostoTareaSimpleComplejidadMedia(CuTest* tc) {
+	float costoDarClase = costoTarea("Medir tiempo clase", 3, COMPLEJIDAD_MEDIA);
+
+	CuAssert(tc, "costo de medir tiempo de clase", costoDarClase == 75);
+}
+
+void TestCostoTareaCompuestaSinImpuestos(CuTest* tc) {
+	tarea darClase = prepareDarClase();
+	float costoDarClase = Tarea_costo(darClase);
+	Tarea_destroy(darClase);
+	//printf("\nTarea de %s \n%4.3f", Tarea_asString(darClase), costoDarClase);
+	//Tarea_verSubtareas(darClase);
+
+	CuAssert(tc, "costo de dar clase", costoDarClase == 400);
+}
+
+void TestCostoProyecto(CuTest* tc) {
+	tarea subirResumenAlSite = Tarea_crear("Subir resumen al site", 10, COMPLEJIDAD_MINIMA);
+	proyecto claseDisenioEstructurado = Proyecto_crear("Disenio estructurado");
+	Proyecto_agregarTarea(claseDisenioEstructurado, prepareDarClase());
+	Proyecto_agregarTarea(claseDisenioEstructurado, subirResumenAlSite);
+	float costoClaseDE = Proyecto_costo(claseDisenioEstructurado);
+	Tarea_destroy(subirResumenAlSite);
+	Proyecto_destroy(claseDisenioEstructurado);
+	CuAssert(tc, "costo de proyecto", costoClaseDE == 650);
+}
+
+CuSuite* CuGetTareasSuite(void) {
+	CuSuite* suite = CuSuiteNew();
+
+	SUITE_ADD_TEST(suite, TestCostoTareaSimpleComplejidadMinima);
+	SUITE_ADD_TEST(suite, TestCostoTareaSimpleComplejidadMinima);
+	SUITE_ADD_TEST(suite, TestCostoTareaCompuestaSinImpuestos);
+	SUITE_ADD_TEST(suite, TestCostoTareaSimpleComplejidadMinimaConImpuestos);
+	SUITE_ADD_TEST(suite, TestCostoProyecto);
+	return suite;
+}
+
